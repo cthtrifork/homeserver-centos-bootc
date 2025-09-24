f8485d1276494e83a9961fb4eec2d5239c0d0075
Casper Thygesen <cth@trifork.com>
2025-09-24 08:23:27 +0000

    fix: update IMAGE_FULL environment variable usage in build and test scripts

--- tests/verify-cosign.sh
+++ tests/verify-cosign.sh
@@ -8,7 +8,8 @@ EOF
 echo "Pulling image: $IMAGE_FULL:latest without signature verification"
 podman --signature-policy /tmp/policy-permissive.json pull ${IMAGE_FULL}:latest
 
-DIGEST=$(skopeo inspect --raw docker://${IMAGE_FULL}:latest | jq -r '.manifests?[0].digest // .digest')
+#DIGEST=$(skopeo inspect --raw docker://${IMAGE_FULL}:latest | jq -r '.manifests?[0].digest // .digest')
+DIGEST=$(skopeo inspect --format "{{.Digest}}" docker://${IMAGE_FULL}:latest)
 
 echo "Manually verifying image signature for image: ${IMAGE_FULL}@${DIGEST}"
 cosign verify --key /etc/pki/cosign/cosign.pub ${IMAGE_FULL}@${DIGEST}
