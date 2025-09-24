b44dbc06808e52f2c2f76747a5353beea5841fc3
Casper thygesen <caspert@gmail.com>
2025-09-24 17:26:36 +0000

    more debugging

--- tests/verify-cosign.sh
+++ tests/verify-cosign.sh
@@ -7,7 +7,6 @@ EOF
 
 echo "Pulling image: ${IMAGE_FULL}:latest without signature verification"
 skopeo copy --policy /tmp/policy-permissive.json docker://${IMAGE_FULL}:latest dir:$(mktemp -d ${HOME}/tmp-image.XXXXXX)
-#podman pull --policy ${IMAGE_FULL}:latest
 
 #DIGEST=$(skopeo inspect --raw docker://${IMAGE_FULL}:latest | jq -r '.manifests?[0].digest // .digest')
 DIGEST=$(skopeo inspect --format "{{.Digest}}" docker://${IMAGE_FULL}:latest)
@@ -15,6 +14,9 @@ DIGEST=$(skopeo inspect --format "{{.Digest}}" docker://${IMAGE_FULL}:latest)
 echo "Manually verifying image signature for image: ${IMAGE_FULL}@${DIGEST}"
 cosign verify --key /etc/pki/cosign/cosign.pub ${IMAGE_FULL}@${DIGEST}
 
+podman image trust show --raw | jq
+
 echo "Image signature verified manually. Pulling image: ${IMAGE_FULL}:latest"
-podman pull ${IMAGE_FULL}@${DIGEST}
-podman pull ${IMAGE_FULL}:latest
+podman pull --signature-policy /tmp/policy-permissive.json ${IMAGE_FULL}@${DIGEST} || true
+podman pull ${IMAGE_FULL}@${DIGEST} || true
+podman pull ${IMAGE_FULL}:latest || true
