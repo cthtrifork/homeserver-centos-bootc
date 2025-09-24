80c48e0ef0226c804e33bf212ec8db9a65e71d5a
Casper Thygesen <cth@trifork.com>
2025-09-24 08:21:03 +0000

    tests/verify-cosign.sh

---
+++ tests/verify-cosign.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+cat > /tmp/policy-permissive.json <<'EOF'
+{ "default": [{"type":"insecureAcceptAnything"}] }
+EOF
+
+echo "Pulling image: $IMAGE_FULL:latest without signature verification"
+podman --signature-policy /tmp/policy-permissive.json pull ${IMAGE_FULL}:latest
+
+DIGEST=$(skopeo inspect --raw docker://${IMAGE_FULL}:latest | jq -r '.manifests?[0].digest // .digest')
+
+echo "Manually verifying image signature for image: ${IMAGE_FULL}@${DIGEST}"
+cosign verify --key /etc/pki/cosign/cosign.pub ${IMAGE_FULL}@${DIGEST}
+
+echo "Image signature verified manually. Pulling image: ${IMAGE_FULL}:latest"
+podman pull ${IMAGE_FULL}:latest
