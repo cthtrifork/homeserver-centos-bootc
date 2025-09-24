82bb18f2df528abbed5acb42af87bd9fea128723
Casper thygesen <caspert@gmail.com>
2025-09-24 18:35:45 +0000

    simplify

--- tests/verify-cosign.sh
+++ tests/verify-cosign.sh
@@ -1,23 +1,26 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-cat > /tmp/policy-permissive.json <<'EOF'
-{ "default": [{"type":"insecureAcceptAnything"}] }
+scopeo_verify() {
+    cat > /tmp/policy-permissive.json <<'EOF'
+    { "default": [{"type":"insecureAcceptAnything"}] }
 EOF
 
-echo "Pulling image: ${IMAGE_FULL}:latest without signature verification"
-skopeo copy --policy /tmp/policy-permissive.json docker://${IMAGE_FULL}:latest dir:$(mktemp -d ${HOME}/tmp-image.XXXXXX)
+    echo "Pulling image: ${IMAGE_FULL}:latest without signature verification"
+    skopeo copy --policy /tmp/policy-permissive.json docker://${IMAGE_FULL}:latest dir:$(mktemp -d ${HOME}/tmp-image.XXXXXX)
+    DIGEST=$(skopeo inspect --format "{{.Digest}}" docker://${IMAGE_FULL}:latest)
+    echo "Manually verifying image signature for image: ${IMAGE_FULL}@${DIGEST}"
+    cosign verify --key /etc/pki/cosign/cosign.pub ${IMAGE_FULL}@${DIGEST}
+    echo "Image signature verified manually"
+}
 
-#DIGEST=$(skopeo inspect --raw docker://${IMAGE_FULL}:latest | jq -r '.manifests?[0].digest // .digest')
-DIGEST=$(skopeo inspect --format "{{.Digest}}" docker://${IMAGE_FULL}:latest)
+podman_pull_without_trust() {
+    cat > /tmp/policy-permissive.json <<'EOF'
+    { "default": [{"type":"insecureAcceptAnything"}] }
+EOF
+    echo "Pulling image: ${IMAGE_FULL}:latest without signature verification"
+    podman pull --signature-policy /tmp/policy-permissive.json ${IMAGE_FULL}@latest
+}
 
-echo "Manually verifying image signature for image: ${IMAGE_FULL}@${DIGEST}"
-cosign verify --key /etc/pki/cosign/cosign.pub ${IMAGE_FULL}@${DIGEST}
-
-podman image trust show --raw | jq
-
-echo "Image signature verified manually. Pulling image: ${IMAGE_FULL}:latest"
-podman pull --signature-policy /tmp/policy-permissive.json ${IMAGE_FULL}@${DIGEST} || true
-podman pull ${IMAGE_FULL}@${DIGEST} || true
-podman pull --log-level=debug ${IMAGE_FULL}:latest 2>&1 | grep -iE 'digest|signature|sigstore' || true
-podman pull ${IMAGE_FULL}:latest || true
+#podman pull --log-level=debug ${IMAGE_FULL}:latest 2>&1 | grep -iE 'digest|signature|sigstore' || true
+podman pull ${IMAGE_FULL}:latest
