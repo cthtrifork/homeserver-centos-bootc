5501d8c811ed8cefe6bfda44f896a3f7d4d10fa9
Casper thygesen <caspert@gmail.com>
2025-09-28 20:20:30 +0000

    simpliy test

--- tests/jumphost/test.sh
+++ tests/jumphost/test.sh
@@ -1,16 +1,5 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-CORE_SERVICES="hosted-terminal.service" # todo
-echo "--- core services ---"
-for s in $CORE_SERVICES; do
-    systemctl is-active --quiet "$s" || {
-    echo "Service not active: $s"
-    systemctl status "$s" --no-pager || true
-    exit 1
-    }
-done
-echo "all ok"
-
 echo "Verifying reverse proxy..."
 curl -iv -u admin:yourpassword http://localhost:8080/
