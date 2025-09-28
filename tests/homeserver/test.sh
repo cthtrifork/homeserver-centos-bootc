3fc191dfb65745c269015f09ccb90112dffabc82
Casper thygesen <caspert@gmail.com>
2025-09-28 19:32:25 +0000

    disable tls for now

--- tests/homeserver/test.sh
+++ tests/homeserver/test.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 CORE_SERVICES="pinggy.service homer-groups.service" # todo
 echo "--- core services ---"
-for s in $ALL_SERVICES; do
+for s in $CORE_SERVICES; do
     systemctl is-active --quiet "$s" || {
     echo "Service not active: $s"
     systemctl status "$s" --no-pager || true
