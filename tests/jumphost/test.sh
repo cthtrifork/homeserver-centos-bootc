f60c059aebbbfafa6955bac1fb622e665bde337e
Casper thygesen <caspert@gmail.com>
2025-09-29 14:40:55 +0000

    Network=host # needed to reach demo-backend

--- tests/jumphost/test.sh
+++ tests/jumphost/test.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 echo "Verifying reverse proxy..."
 
 sudo tree /var/lib/caddy-demo
-curl -iv -u admin:yourpassword http://localhost:8080/
+curl -iv -u admin:yourpassword http://localhost:8080/health
