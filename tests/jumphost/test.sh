ec4c5a1efe036abfdd29bfd549930a103519f503
Casper Thygesen <cth@trifork.com>
2025-10-01 08:34:01 +0000

    Continued work on testing jumphost

--- tests/jumphost/test.sh
+++ tests/jumphost/test.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 echo "Verifying reverse proxy..."
 
-#sudo tree /var/lib/caddy-demo
+sudo tree /var/lib/caddy-demo
 resp=$(curl -u admin:yourpassword -s -w "%{http_code}" http://localhost:8080/health)
 body=${resp::-3}
 code=${resp: -3}
