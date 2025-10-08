70af59882bd9924e7aee1b3d7d4a6b984c846edc
Casper thygesen <caspert@gmail.com>
2025-10-08 22:20:42 +0200

    systemctl enable homed-firstboot-users.service

--- build_files/systemd.sh
+++ build_files/systemd.sh
@@ -15,6 +15,6 @@ systemctl enable podman.socket
 systemctl enable systemd-userdbd.service
 systemctl enable systemd-homed.service 
 systemctl enable sshd.service
-#systemctl enable homed-firstboot-users.service
+systemctl enable homed-firstboot-users.service
 systemctl enable homer-groups.service
 systemctl enable pinggy.service
