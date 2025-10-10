abe2a4461cbef38a8379c2af351fe98e4e2f73cc
Casper Thygesen <cth@trifork.com>
2025-10-10 08:22:04 +0200

    use personal user

--- tests/homeserver/test.sh
+++ tests/homeserver/test.sh
@@ -11,14 +11,14 @@ for s in $CORE_SERVICES; do
     exit 1
     }
 done
-echo "all ok"
+echo "✅ core services are OK"
 
 #/usr/bin/systemd-tmpfiles --cat-config
 
-echo "== caspertdk: home directory exists =="
-sudo test -d /home/caspertdk && echo "✅ home directory exists" || { echo "❌ home directory missing"; exit 1; }
-sudo tree -uag /home/caspertdk 
-sudo test -f /home/caspertdk/.ssh/authorized_keys && echo "✅ authorized_keys exists" || { echo "❌ authorized_keys missing"; exit 1; }
+echo "== caspertdk: home directory is valid =="
+test -d /home/caspertdk && echo "✅ home directory exists" || { echo "❌ home directory missing"; exit 1; }
+tree -uag /home/caspertdk 
+test -f /home/caspertdk/.ssh/authorized_keys && echo "✅ authorized_keys exists" || { echo "❌ authorized_keys missing"; exit 1; }
 echo "✅ homed user creation + authorized keys OK"
 
 # check if env var ENV_LOAD is loaded
@@ -26,13 +26,12 @@ if [ -z "${ENV_LOAD:-}" ]; then
     echo "ENV_LOAD is not set"
     exit 1
 fi
-echo "ENV_LOAD is set to '$ENV_LOAD'. /etc/profile.d is working as intended."
+echo "✅ ENV_LOAD is set to '$ENV_LOAD'. /etc/profile.d is working as intended."
 
 echo "Checking if user is in docker group"
-docker run --rm hello-world
+docker run --rm hello-world && echo "✅ Docker is ready"
 
 echo "Checking github Auth status"
+echo "GitHub token fingerprint: $(printf "%s" "$GITHUB_TOKEN" | cut -c1-7)"
 
-echo "GitHub token fingerprint: $(printf "%s" "$GITHUB_TOKEN" | cut -c1-6)"
-
-gh auth status
+gh auth status && echo "✅ Github is ready"
