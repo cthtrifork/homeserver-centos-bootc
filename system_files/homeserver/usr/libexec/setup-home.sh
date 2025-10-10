00fd21c6315c7822d6aa9516613a6c49c86bec35
Casper Thygesen <cth@trifork.com>
2025-10-10 15:42:08 +0200

    simplify

--- system_files/homeserver/usr/libexec/setup-home.sh
+++ system_files/homeserver/usr/libexec/setup-home.sh
@@ -9,34 +9,45 @@ fi
 USER="$1"
 HOME_DIR="/home/$USER"
 
-cd $HOME_DIR
+cd "$HOME_DIR"
 
 # Setup OH-MY-BASH for user
-if [[ ! -d "${HOME_DIR}/.dotfiles/oh-my-bash" ]]; then
-    export OSH="$HOME_DIR/.dotfiles/oh-my-bash"; bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)" --unattended
+install_ohmybash() {
+  if [[ ! -f "/usr/local/share/oh-my-bash/bashrc" ]]; then
+      # systemwide
+      bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)" --prefix=/usr/local --unattended
+      # copy local
+      cp /usr/local/share/oh-my-bash/bashrc "$HOME_DIR"/.bashrc
 
-    # modify .bashrc
-    # https://github.com/ohmybash/oh-my-bash?tab=readme-ov-file
-    sed -i 's/^plugins=(git)$/plugins=(git kubectl)/g' $HOME_DIR/.bashrc
-    echo 'export OMB_USE_SUDO=false' >> $HOME_DIR/.bashrc
-    echo 'export DISABLE_AUTO_UPDATE=true' >> $HOME_DIR/.bashrc
-fi
+      # modify .bashrc
+      # https://github.com/ohmybash/oh-my-bash?tab=readme-ov-file
+      sed -i 's/^plugins=(git)$/plugins=(git kubectl)/g' "$HOME_DIR"/.bashrc
+      echo 'export OMB_USE_SUDO=false' >> "$HOME_DIR"/.bashrc
+      echo 'export DISABLE_AUTO_UPDATE=true' >> "$HOME_DIR"/.bashrc
+  fi
+  sudo chown "$USER":"$USER" "$HOME_DIR"/.bashrc
+}
 
 # Setup .gitconfig for user
-if [[ -f "/home/$USER/.ssh/id_ed25519.pub" ]]; then
-  cat <<EOT | tee /home/$USER/.gitconfig > /dev/null
-[user]
-    name = Casper Thygesen
-    email = cth@trifork.com
-    signingkey = /home/$USER/.ssh/id_ed25519.pub
-[gpg]
-    format = ssh
-[commit]
-    gpgsign = true
-[init]
-    defaultBranch = main
+configure_gitconfig() {
+  if [[ -f "/home/$USER/.ssh/id_ed25519.pub" ]]; then
+    cat <<EOT | tee /home/"$USER"/.gitconfig > /dev/null
+  [user]
+      name = Casper Thygesen
+      email = cth@trifork.com
+      signingkey = /home/$USER/.ssh/id_ed25519.pub
+  [gpg]
+      format = ssh
+  [commit]
+      gpgsign = true
+  [init]
+      defaultBranch = main
 EOT
-  chown $USER:$USER /home/$USER/.gitconfig
-fi
+    sudo chown "$USER":"$USER" "$HOME_DIR"/.gitconfig
+  fi
+}
+
+install_ohmybash
+configure_gitconfig
 
 echo "Finished setting up home for $USER"
