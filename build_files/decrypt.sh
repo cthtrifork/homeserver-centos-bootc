0dd281fe92b8fedcf1b8c0863d7dc8346aeee1d1
casper thygesen <caspert@gmail.com>
2025-10-14 22:40:17 +0200

    again

--- build_files/decrypt.sh
+++ build_files/decrypt.sh
@@ -15,7 +15,10 @@ if [[ ! -f "$SOPS_AGE_KEY_FILE" ]]; then
   exit 1
 fi
 
+export SOPS_AGE_KEY="$(cat $SOPS_AGE_KEY_FILE)"
+
 log "Decrypting SSH key"
 /usr/bin/install -d -m 0700 /var/lib/secrets/ssh
-sops --decrypt --output /var/lib/secrets/ssh/id_ed25519 /usr/share/secrets/ssh/id_ed25519.sops
+sops --decrypt --input-type binary --output-type binary --output /var/lib/secrets/ssh/id_ed25519 /usr/share/secrets/ssh/id_ed25519.sops
 chmod 600 /var/lib/secrets/ssh/id_ed25519
+
