a97b1f54ebec160f997dfdabe5fe99f020562d0f
Casper Thygesen <cth@trifork.com>
2025-10-15 09:10:55 +0200

    Update decrypt.sh

--- build_files/decrypt.sh
+++ build_files/decrypt.sh
@@ -8,15 +8,15 @@ log() {
   echo "=== $* ==="
 }
 
-export SOPS_AGE_KEY_FILE="/usr/lib/sops/age/keys.txt"
+export SOPS_AGE_KEY_FILE="/usr/share/sops/age/keys.txt"
 
 if [[ ! -f "$SOPS_AGE_KEY_FILE" ]]; then
   echo "Error: SOPS_AGE_KEY_FILE ($SOPS_AGE_KEY_FILE) does not exist" >&2
   exit 1
 fi
 
-grep -q '^AGE-SECRET-KEY-1' /usr/lib/sops/age/keys.txt || { echo "Not an age key file"; exit 1; }
+grep -q '^AGE-SECRET-KEY-1' /usr/share/sops/age/keys.txt || { echo "Not an age key file"; exit 1; }
 
 log "Decrypting SSH key"
-sops --decrypt --input-type binary --output-type binary --output /usr/lib/secrets/ssh/id_ed25519 /usr/lib/secrets/ssh/id_ed25519.enc
-chmod 600 /usr/lib/secrets/ssh/id_ed25519
+sops --decrypt --input-type binary --output-type binary --output /usr/share/secrets/ssh/id_ed25519 /usr/share/secrets/ssh/id_ed25519.enc
+chmod 600 /usr/share/secrets/ssh/id_ed25519
